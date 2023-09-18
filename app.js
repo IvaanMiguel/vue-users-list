ac37d066a3521cd70316f623f79e9ce0bd9fdd9f
IvaanMiguel <92279628+IvaanMiguel@users.noreply.github.com>
2023-09-18 09:38:54 -0700

    Login básico

--- app.js
+++ app.js
@@ -2,25 +2,46 @@ const { createApp, ref } = Vue
 
 createApp({
     setup() {
-        const username = ''
-        const name = ''
-        const users = ref([])
+        // const username = ''
+        // const name = ''
+        const email = ''
+        const password = ''
+        // const users = ref([])
 
         return {
-            username,
-            name,
-            users
+            // username,
+            // name,
+            email,
+            password,
+            // users
         }
     },
 
     methods: {
-        addUser() {
-            if (!this.username || !this.name) return
+        login(e) {
+            e.preventDefault()
 
-            this.users.push({
-                username: this.username,
-                name: this.name
-            })
+            fetch("users.json")
+                .then(response => response.json())
+                .then(users => {
+                    const userFound = users.find(user => user.email === this.email && user.password === this.password)
+
+                    alert(userFound ? 'Bienvenido.' : 'El email o la contraseña no coinciden.')
+
+                    if (!userFound) return
+
+                    console.log('Guardando datos.')
+                })
+
+            // if (!this.username || !this.name) return
+
+            // this.users.push({
+            //     username: this.username,
+            //     name: this.name
+            // })
+
+            // this.username = ""
+            // this.name = ""
         }
     }
 }).mount("#app")
