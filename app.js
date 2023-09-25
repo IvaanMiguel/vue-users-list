1346b920d8732c6509d3b0784d9d936fbc52b044
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2023-09-24 21:21:53 -0700

    Actividad 13.

--- app.js
+++ app.js
@@ -2,26 +2,24 @@ const { createApp, ref } = Vue
 
 createApp({
     setup() {
-        // const username = ''
-        // const name = ''
-        const email = 'Sincere@april.biz'
+        const userForm = ref({})
+        const loginEmail = 'Sincere@april.biz'
         const password = '1234'
         const isLogged = ref(localStorage.getItem('isLogged') ?? false)
         const usersList = ref([])
-        // const users = ref([])
+        const isInUserForm = ref(false)
 
         return {
-            // username,
-            // name,
-            email,
+            loginEmail,
             password,
             isLogged,
-            usersList
-            // users
+            usersList,
+            isInUserForm,
+            userForm
         }
     },
 
-    mounted: function() {
+    mounted() {
         if (!this.isLogged) return
 
         fetch("users.json")
@@ -30,34 +28,100 @@ createApp({
     },
 
     methods: {
-        login(e) {
-            e.preventDefault()
-
+        login() {
             fetch("users.json")
                 .then(response => response.json())
                 .then(users => {
-                    const userFound = users.find(user => user.email === this.email && user.password === this.password)
+                    const userFound = users.find(user => user.email === this.loginEmail && user.password === this.password)
 
                     this.isLogged = userFound !== undefined
 
                     alert(this.isLogged ? 'Bienvenido.' : 'El email o la contraseña no coinciden.')
 
-                    if (!this.isLogged) return
+                    if (!this.isLogged) return  
 
                     localStorage.setItem('userData', JSON.stringify(userFound))
                     localStorage.setItem('isLogged', this.isLogged)
                     this.usersList = users
                 })
+        },
 
-            // if (!this.username || !this.name) return
+        cancelUserForm() {
+            this.isInUserForm = false
 
-            // this.users.push({
-            //     username: this.username,
-            //     name: this.name
-            // })
+            this.userForm = {}
+        },
 
-            // this.username = ""
-            // this.name = ""
-        }
+        createUser() {
+            this.isInUserForm = true
+        },
+
+        storeUser() {
+            if (Object.values(this.userForm).length < 6) return
+
+            // Cortesía de ChatGPT.
+            const userId = this.usersList.reduce((maxId, user) => user.id > maxId ? user.id : maxId, 0)
+
+            this.userForm.id = userId + 1
+            this.usersList.push(this.userForm)
+            this.isInUserForm = false
+
+            this.userForm = {}
+        },
+
+        editUser(e) {
+            const userId = +e.target.closest('tr').querySelector('input').value
+            const user = this.usersList.find(user => user.id === userId)
+
+            this.userForm = {
+                id: user.id,
+                username: user.username,
+                name: user.name,
+                email: user.email,
+                city: user.city ?? user.address.city,
+                street: user.street ?? user.address.street,
+                phone: user.phone
+            }
+
+            this.isInUserForm = true
+        },
+
+        updateUser() {
+            const user = this.usersList.find(user => user.id === this.userForm.id)
+
+            user.username = this.userForm.username
+            user.email = this.userForm.email
+            user.name = this.userForm.name
+
+            if (user.address) {
+                user.address.city = this.userForm.city
+                user.address.street = this.userForm.street
+            } else {
+                user.city = this.userForm.city
+                user.street = this.userForm.street
+            }
+
+            user.phone = this.userForm.phone
+
+            this.userForm = {}
+
+            this.isInUserForm = false
+        },
+
+        destroyUser(e) {
+            const userId = +e.target.closest('tr').querySelector('input').value
+            const user = this.usersList.find(user => user.id === userId)
+            const index = this.usersList.indexOf(user)
+
+            if (JSON.parse(localStorage.getItem('userData')).id === user.id) {
+                alert('Cannot delete your own user.')
+                return
+            }
+
+            this.usersList.splice(index, 1)
+
+            alert(`${user.name} deleted.`)
+        },
+
     }
 }).mount("#app")
